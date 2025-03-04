509aa23909ddba9e131dfbf042c7e43a0f1f75f2
ThibTP <115563685+ThibTP@users.noreply.github.com>
2025-03-04 19:20:24 +0100

    Update stg_dim_aeroplane.sql

--- air-boltic-elt_final/dbt/models/staging/stg_dim_aeroplane.sql
+++ air-boltic-elt_final/dbt/models/staging/stg_dim_aeroplane.sql
@@ -3,7 +3,7 @@ SELECT
     a.airplane_model AS airplane_model_id, 
     a.manufacturer, 
     am.engine_type,
-    CURRENT_TIMESTAMP AS created_at,
+    CURRENT_DATE AS created_at,
     CURRENT_TIMESTAMP AS updated_at
 FROM {{ source("airboltic-dwh", "aeroplane") }} a
 LEFT JOIN {{ source("airboltic-dwh", "aeroplane_model") }} am
