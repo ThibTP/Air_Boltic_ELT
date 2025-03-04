f431015a3f9ae518a0ddfb4a4535f151c8515337
ThibTP <115563685+ThibTP@users.noreply.github.com>
2025-03-04 18:48:43 +0100

    Update stg_dim_trip.sql

--- air-boltic-elt_final/dbt/models/staging/stg_dim_trip.sql
+++ air-boltic-elt_final/dbt/models/staging/stg_dim_trip.sql
@@ -1,12 +1,10 @@
 SELECT 
-    t.trip_id, 
-    t.airplane_id, 
-    d.origin_city, 
-    d.destination_city, 
-    t.start_timestamp, 
-    t.end_timestamp,
+    trip_id, 
+    origin_city, 
+    destination_city, 
+    airplane_id, 
+    start_timestamp, 
+    end_timestamp,
     CURRENT_TIMESTAMP AS created_at,
     CURRENT_TIMESTAMP AS updated_at
-FROM {{ source("airboltic-dwh", "trip") }} t
-LEFT JOIN {{ source("airboltic-dwh", "aeroplane") }} a
-    ON t.airplane_id = a.airplane_id
+FROM {{ source("airboltic-dwh", "trip") }}
